d185f67062a2adbce4bc1ad8d8c56064f1b7be9d
Boris Bügling <boris@icculus.org>
2015-12-08 10:42:35 +0100

    Parse the given spec

--- lib/cocoapods-schoutedenapus/command/schoutedenapus.rb
+++ lib/cocoapods-schoutedenapus/command/schoutedenapus.rb
@@ -7,17 +7,35 @@ module Pod
         self.arguments = [ CLAide::Argument.new('PODSPEC', true) ]
 
         def initialize(argv)
-          @name = argv.shift_argument
+          @spec = spec_with_path(argv.shift_argument)
           super
         end
 
         def validate!
           super
-          help! 'A Pod name is required.' unless @name
+          help! 'A podspec is required.' unless @spec
         end
 
         def run
           UI.puts "Add your implementation for the cocoapods-schoutedenapus plugin in #{__FILE__}"
+        end
+
+        private
+
+        def spec_with_path(path)
+          return if path.nil? || !Pathname.new(path).exist?
+
+          if Pathname.new(path).directory?
+            help! path + ': is a directory.'
+            return
+          end
+
+          unless ['.podspec', '.json'].include? Pathname.new(path).extname
+            help! path + ': is not a podspec.'
+            return
+          end
+
+          Specification.from_file(path)
         end
       end
     end
--- spec/command/schoutedenapus_spec.rb
+++ spec/command/schoutedenapus_spec.rb
@@ -6,6 +6,13 @@ module Pod
       it 'registers it self' do
         Command.parse(%w( lib schoutedenapus )).should.be.instance_of Command::Lib::Schoutedenapus
       end
+
+      it 'parses a given spec' do
+        cmd = Command.parse(%w( lib schoutedenapus spec/fixtures/Stencil.podspec.json ))
+        cmd.validate!
+
+        cmd.instance_variable_get('@spec').name.should == 'Stencil'
+      end
     end
   end
 end
